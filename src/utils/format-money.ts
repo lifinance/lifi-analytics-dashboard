90803e146a0fc31963fe0d6a4143b9c51347aab9
Michael Pautov <mpautov@deem.com>
2021-10-12 16:45:55 +0600

    fix(LIFI-13): changed volume format

--- src/utils/format-money.ts
+++ src/utils/format-money.ts
@@ -1,27 +1,12 @@
-import { insertMorpheme } from '@lifi/utils/insert-morpheme'
-import { roundToDecimal } from '@lifi/utils/round-to-decimals'
-
 export function formatMoney(
   amount: number,
   options?: {
     hideDecimal?: boolean
-    prefix?: boolean
-    locale?: string
   },
 ): string {
-  const { locale, hideDecimal, prefix } = options || {}
-
-  const params = {
+  return amount.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
-    ...(hideDecimal && { minimumFractionDigits: 0, maximumFractionDigits: 0 }),
-  }
-
-  const usedLocale = locale || window.navigator.language
-
-  const value = hideDecimal ? amount : roundToDecimal(amount)
-
-  const formattedMoney = new Intl.NumberFormat(usedLocale, params).format(value)
-
-  return insertMorpheme(formattedMoney, 'USD', prefix ? 'prefix' : undefined)
+    ...(options?.hideDecimal && { minimumFractionDigits: 0, maximumFractionDigits: 0 }),
+  })
 }
